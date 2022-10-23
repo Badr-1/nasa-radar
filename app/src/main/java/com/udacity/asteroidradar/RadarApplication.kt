32e67027ab89fcfa9137ed0155aa82e2858d3893
Badr <badrmohsamedashraf@gmail.com>
2022-10-23 22:19:37 +0200

    adding constraints to delete worker

--- app/src/main/java/com/udacity/asteroidradar/RadarApplication.kt
+++ app/src/main/java/com/udacity/asteroidradar/RadarApplication.kt
@@ -1,15 +1,14 @@
 package com.udacity.asteroidradar
 
 import android.app.Application
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
+import android.os.Build
+import androidx.work.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.util.concurrent.TimeUnit
 
-class RadarApplication:Application() {
+class RadarApplication : Application() {
 
     private val applicationScope = CoroutineScope(Dispatchers.Default)
 
@@ -23,11 +22,23 @@ class RadarApplication:Application() {
     }
 
     private fun setupRecurringWork() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }.build()
+
         val repeatingRequest = PeriodicWorkRequestBuilder<DeletePastDaysWorker>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
             .build()
         WorkManager.getInstance().enqueueUniquePeriodicWork(
             DeletePastDaysWorker.WORK_NAME,
             ExistingPeriodicWorkPolicy.KEEP,
-            repeatingRequest)
+            repeatingRequest
+        )
     }
 }
